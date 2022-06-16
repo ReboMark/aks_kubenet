80ba15fea1a4d94b503fd41bf2058dc72f787833
Rebo Mark Agustines <rebo.agustines@ext.essilor.com>
2022-06-17 00:57:57 +0800

    Deploy AKS

--- coreapp3/Program.cs
+++ coreapp3/Program.cs
@@ -25,11 +25,10 @@ builder.Services.AddMediatR(typeof(GetUserQueryHandler));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
