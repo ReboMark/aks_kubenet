d68f8114c468e5dc5947c330a087435be7b37ee0
Rebo Mark Agustines <rebo.agustines@ext.essilor.com>
2022-06-17 16:44:47 +0800

    add health checks

---
+++ coreapp3/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreapp3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public HealthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("/health")]
+        [AllowAnonymous]
+        public string Get()
+        {
+            return $"{DateTime.Now.ToUniversalTime().ToString("yyyy/MM/dd HH:mm:ss")} Coreapp3 Service";
+        }
+    }
+}
